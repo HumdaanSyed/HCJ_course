fc068bbf454ee2e4a23aa5e2f21860532dd29d60
Humdaan Syed <humdaansyed@gmail.com>
2021-06-25 17:53:25 +0530

    Added Module 4 solution

---
+++ site/js/script.js
@@ -0,0 +1,4 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var firstLetter = names[0].charAt(0);
+var result = firstLetter.toLowerCase;
+console.log(firstLetter);
